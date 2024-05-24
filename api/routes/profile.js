bd4237c029f4499c009a86b6213c0bd0777672fe
Dewangga <hamadakinose@gmail.com>
2024-05-24 20:42:56 +0700

    Profile Documentation Done

--- api/routes/profile.js
+++ api/routes/profile.js
@@ -3,7 +3,7 @@ const { getProfile, editProfile } = require("../controllers/profile.controller")
 const { middleware } = require("../middleware/middleware")
 const router = express.Router();
 
-router.get("/:user_id", getProfile)
+router.get("/:user_id", middleware, getProfile)
 router.put("/:user_id", middleware, editProfile)
 
 module.exports = router;
