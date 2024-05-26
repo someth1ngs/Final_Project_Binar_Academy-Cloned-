41e09f7c03978f7fc04db5b92c344d221b3ac4da
ketutk <ketutkrisnakertajaya@gmail.com>
2024-05-26 15:56:14 +0700

    add flight seeders and date-fns

--- prisma/seeders/flight-seed.js
+++ prisma/seeders/flight-seed.js
@@ -1,57 +1,91 @@
-// const { PrismaClient } = require("@prisma/client");
-// const prisma = new PrismaClient();
-// async function FlightSeeder() {
-//   return new Promise(async (resolve, reject) => {
-//     let data = [
-//       {
-//         plane_code:'',
-//         from_code:"",
-//         to_code:"",
-//         departureAt:Date.now(),
-//         arriveAt:Date.now(),
-//         is_return:true,
-//         return_departureAt:
-//         return_arriveAt:
-//         flight_type:"DOMESTIC"
-//       },
-//       {
-//         plane_code:'',
-//         from_code:"",
-//         to_code:"",
-//         departureAt:Date.now(),
-//         arriveAt:Date.now(),
-//         is_return:true,
-//         return_departureAt:
-//         return_arriveAt:
-//         flight_type:"INTERNATIONAL",
-//         flight_classes:{
-//             create:[
-//                 {
-//                     name:"ECONOMY",
-//                     price:1500000,
-//                     availabe_seats: 50,
-//                 }
-//             ]
-//         }
-//       },
-//     ];
-//     try {
-//       const deleteFlight = await prisma.flight.deleteMany({});
-//       const flightCreate = await prisma.flight.create({
-//         data:{
-//             plane:{
-//                 create:[
-//                     {}
-//                 ]
-//             }
+const { PrismaClient } = require("@prisma/client");
+const { addingDays } = require("../../api/libs/date-fns");
+const prisma = new PrismaClient();
+async function FlightSeeder() {
+  return new Promise(async (resolve, reject) => {
+    const departureAt = addingDays(30, 10, 5);
+    const arriveAt = addingDays(30, 11, 20);
+    const return_departureAt = addingDays(37, 12, 10);
+    const return_arriveAt = addingDays(30, 13, 5);
+    try {
+      const deleteFlight = await prisma.flight.deleteMany({});
+      const generateFlightData = (planeData, airportData) => {
+        const flightData = [];
 
-//         }
-//       })
-//       resolve("Success create category seeds");
-//     } catch (error) {
-//       reject(error.message);
-//     }
-//   });
-// }
+        planeData.forEach((plane) => {
+          // Choose random airports
+          const fromAirport = airportData[Math.floor(Math.random() * airportData.length)];
+          let toAirport;
+          do {
+            toAirport = airportData[Math.floor(Math.random() * airportData.length)];
+          } while (toAirport.airport_code === fromAirport.airport_code);
 
-// module.exports = FlightSeeder;
+          // Determine if it's a return flight
+          const isReturn = Math.random() < 0.5; // 50% chance of being a return flight
+
+          // Determine flight type
+          const flightType = fromAirport.country === toAirport.country ? "DOMESTIC" : "INTERNATIONAL";
+
+          flightData.push({
+            plane_code: plane.plane_code,
+            from_code: fromAirport.airport_code,
+            to_code: toAirport.airport_code,
+            departureAt: departureAt,
+            arriveAt: arriveAt,
+            is_return: isReturn,
+            return_departureAt: isReturn ? return_departureAt : null,
+            return_arriveAt: isReturn ? return_arriveAt : null,
+            flight_type: flightType,
+          });
+        });
+
+        return flightData;
+      };
+      const planeData = await prisma.plane.findMany({});
+      const airportData = await prisma.airport.findMany({});
+      const flightData = generateFlightData(planeData, airportData);
+
+      for (item of flightData) {
+        const getPlaneData = await prisma.plane.findUnique({
+          where: {
+            plane_code: item.plane_code,
+          },
+        });
+        const flightCreate = await prisma.flight.create({
+          data: {
+            ...item,
+            flight_classes: {
+              create: [
+                {
+                  name: "ECONOMY",
+                  price: 1000000,
+                  available_seats: getPlaneData.economy_seat,
+                },
+                {
+                  name: "PREMIUM_ECONOMY",
+                  price: 2000000,
+                  available_seats: getPlaneData.premium_economy_seat,
+                },
+                {
+                  name: "BUSINESS",
+                  price: 5000000,
+                  available_seats: getPlaneData.business_seat,
+                },
+                {
+                  name: "FIRST_CLASS",
+                  price: 10000000,
+                  available_seats: getPlaneData.first_class_seat,
+                },
+              ],
+            },
+          },
+        });
+      }
+      resolve("Success create flight seeds");
+    } catch (error) {
+      reject(error.message);
+    }
+  });
+}
+
+module.exports = FlightSeeder;
--- prisma/seeders/seed.js
+++ prisma/seeders/seed.js
@@ -4,6 +4,7 @@ const CategorySeeder = require("./category-seed");
 const TaxSeeder = require("./tax-seed");
 const AirportSeeder = require("./airport-seed");
 const PlaneSeeder = require("./plane-seed");
+const FlightSeeder = require("./flight-seed");
 const prisma = new PrismaClient();
 
 async function main() {
@@ -12,12 +13,14 @@ async function main() {
   const createTax = await TaxSeeder();
   const createAirport = await AirportSeeder();
   const createPlane = await PlaneSeeder();
+  const createFlight = await FlightSeeder();
 
   console.log(createUser);
   console.log(createCategory);
   console.log(createTax);
   console.log(createAirport);
   console.log(createPlane);
+  console.log(createFlight);
 }
 main()
   .then(async () => {
